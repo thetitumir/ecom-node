3800e85d5ff6325ea3f0b1b8bdeb4539c814e3ca
Sharif <developersharif@yahoo.com>
2023-05-19 19:08:55 +0600

    controller separeted

--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,15 @@
 var express = require('express');
-var User = require("../models/UserModel");
 const EmailCheck = require("../middleware/EmailCheck");
 const Auth = require("../middleware/Auth");
 const Login = require("../middleware/Login");
+const indexController = require("../controller/Auth");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', Auth,function(req, res, next) {
-  const user = req.session.user;
-  res.render('frontend/home',{user: user});
-});
-router.get('/login', function(req, res){
-res.render('frontend/login');
-});
+router.get('/', Auth,indexController.indexController);
+router.get('/login', indexController.loginController);
 router.post('/login', Login);
-
-router.get('/register', function(req, res){
-  res.render('frontend/register');
-})
-router.post('/register',EmailCheck, async function(req, res){
-  const body = req.body;
-  if(body.password && body.cpassword && body.password===body.cpassword) {
-   await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-      status: "active"
-    }).then(user=>{
-      req.session.user = user;
-      res.redirect("/")
-    }).catch((err)=>res.json(err));
-  }else{
-    res.json({message:"Password does not mache"})
-  }
-
-  
-})
-
+router.get('/register', indexController.registerController);
+router.post('/register',EmailCheck, indexController.newRegisterController)
 
 module.exports = router;
