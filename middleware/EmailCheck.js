4f1ccdc58940ea3beeba628c2182974b2dc4f34a
Sharif <developersharif@yahoo.com>
2023-05-19 19:09:27 +0600

    validation added

--- middleware/EmailCheck.js
+++ middleware/EmailCheck.js
@@ -2,7 +2,7 @@ const User = require("../models/UserModel");
 async function EmailCheck(req,res,next) {
     const user = await User.findOne({email:req.body.email});
     if (user) {
-        return res.json({message:"User already exists"})
+        return res.render("frontend/register",{errors:[{path:"Error","msg":"Mail already exists"}]})
     }
     next();
 }
--- middleware/Login.js
+++ middleware/Login.js
@@ -1,12 +1,26 @@
+const {body,validationResult} = require('express-validator');
 const User = require("../models/UserModel");
+const validationRules = [
+  body("email").trim().isEmail().withMessage("Please enter your email"),
+  body("password").trim().notEmpty().withMessage("Please enter your password")
+];
+
 async function Login(req,res,next) {
+
+    await Promise.all(validationRules.map(rule=>rule.run(req)));
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+     
+      return res.render("frontend/login",{errors:errors.errors});
+    }
     const user = await User.findOne({email:req.body.email,password:req.body.password});
     if (user) {
         req.session.user = user;
        return res.redirect("/");
         
     }else{
-      return  res.redirect("/login");
+      return  res.render("frontend/login",{errors:[{path:"Error",msg:"Email or password invalid"}]});
     }
     
 }
